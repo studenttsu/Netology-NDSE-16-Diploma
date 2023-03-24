bb14b573820ca9e02f72bc66b8718cb905617519
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-03-25 01:11:30 +0700

    prepared reservations controller

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
-@ApiTags('Брони')
-@Controller('reservations')
-export class ReservationsController {}
+@ApiTags('Бронирование')
+@Controller()
+export class ReservationsController {
+    @Post('/client/reservations')
+    @ApiOperation({ summary: 'Создаёт бронь на номер на выбранную дату для текущего пользователя' })
+    createReservation() {}
+
+    @Get('/client/reservations')
+    @ApiOperation({ summary: 'Список броней текущего пользователя' })
+    getReservations() {}
+
+    @Get('/manager/reservations/:userId')
+    @ApiOperation({ summary: 'Список броней конкретного пользователя' })
+    getUserReservations(@Param('userId', ParseIntPipe) userId: number) {}
+
+    @Delete('/client/reservations/:id')
+    @ApiOperation({ summary: 'Отменяет бронь пользователя' })
+    removeReservation(@Param('id', ParseIntPipe) id: number) {}
+
+    @Delete('/manager/reservations/:id')
+    @ApiOperation({ summary: 'Отменяет бронь пользователя' })
+    removeReservationMyManager(@Param('id', ParseIntPipe) id: number) {}
+}
