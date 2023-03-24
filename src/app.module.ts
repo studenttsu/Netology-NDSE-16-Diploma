4e13f7ce5825e50c00c600876e3ef9c2ecd3ebb0
Konstantin Teryohin <konstantin.teryohin@rubius.com>
2023-03-24 23:18:13 +0700

    base app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { HotelsModule } from './hotels/hotels.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { SupportModule } from './support/support.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    HotelsModule,
+    ReservationsModule,
+    SupportModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
